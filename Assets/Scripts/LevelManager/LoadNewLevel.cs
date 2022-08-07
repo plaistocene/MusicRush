24dfcd48bb101736db1fb77b3d5177c3106ee3ae
I. Bugra Aslan <46376529+plaistocene@users.noreply.github.com>
2022-08-07 15:31:14 +0300

    LoadNewLevel script created

---
+++ Assets/Scripts/LevelManager/LoadNewLevel.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class LoadNewLevel : MonoBehaviour
+{
+    #region Variables
+
+    #endregion
+
+    #region Unity Methods
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+    
+    #endregion
+}
