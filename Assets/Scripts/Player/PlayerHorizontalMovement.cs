7bfe7e3ca34f360ce3055f56e63eab6fd400c068
I. Bugra Aslan <46376529+plaistocene@users.noreply.github.com>
2022-08-07 15:14:08 +0300

    player horizontal movement now uses mouse button 0

--- Assets/Scripts/Player/PlayerHorizontalMovement.cs
+++ Assets/Scripts/Player/PlayerHorizontalMovement.cs
@@ -4,45 +4,70 @@ public class PlayerHorizontalMovement : MonoBehaviour
 {
     #region Variables
 
-    public PlayerVelocityController playerVelocityController;
-    public bool right;
-    public bool left;
+    public PlayerStats playerStats;
+    public bool moveByTouch;
+
+
+    private Vector3 _mouseStartPos, _playerStartPos;
+    private Camera _camera;
 
     #endregion
 
     #region Unity Methods
 
-    // Start is called before the first frame update
     private void Start()
     {
-        playerVelocityController = GetComponent<PlayerVelocityController>();
+        playerStats = GameObject.Find("PlayerStats").GetComponent<PlayerStats>();
+        _camera = GameObject.Find("Camera").GetComponent<Camera>();
+        playerStats.CalculateMovementLimitX();
     }
 
-    // Update is called once per frame
+
     private void Update()
     {
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetMouseButtonDown(0))
         {
-            left = true;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            right = true;
-        }
-    }
+            moveByTouch = true;
+            PlayerStats.StartGame();
 
-    private void FixedUpdate()
-    {
-        if (right)
-        {
-            right = false;
-            playerVelocityController.MoveRight(Time.fixedDeltaTime);
+            var newPlane = new Plane(Vector3.up, 0f);
+            var ray = _camera.ScreenPointToRay(Input.mousePosition);
+
+            if (newPlane.Raycast(ray, out var distance))
+            {
+                _mouseStartPos = ray.GetPoint(distance);
+                _playerStartPos = new Vector3(0, 5, 31);
+            }
         }
 
-        if (left)
+        if (Input.GetMouseButtonUp(0))
         {
-            left = false;
-            playerVelocityController.MoveLeft(Time.fixedDeltaTime);
+            moveByTouch = false;
+        }
+
+        if (moveByTouch)
+        {
+            var plane = new Plane(Vector3.up, 0f);
+
+            var ray = _camera.ScreenPointToRay(Input.mousePosition);
+
+            if (plane.Raycast(ray, out var distance))
+            {
+                Vector3 mousePos = ray.GetPoint(distance);
+                Vector3 desirePos = mousePos - _mouseStartPos;
+                Vector3 move = _playerStartPos + desirePos;
+
+                move.x = Mathf.Clamp(move.x, -playerStats.GetMovementLimitX(), playerStats.GetMovementLimitX());
+                move.z = 0f;
+
+                var player = transform.position;
+
+                var xPos = Mathf.Lerp(player.x, move.x, Time.deltaTime * playerStats.GetHorizontalSpeed());
+
+                player = new Vector3(xPos, player.y, player.z);
+
+                transform.position = player;
+            }
         }
     }
 
